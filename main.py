c01785033ff8fdb12fabd6c3362ac89569a95158
Arshey123 <aseemsharma1980@gmail.com>
2024-10-09 20:17:28 +0530

    added activity 3

--- main.py
+++ main.py
@@ -4,9 +4,15 @@
 # file= open("text.txt","a")
 # file.write("Beyblad x is coming")
 # activity 2
-file= open("text.txt","r")
+# file= open("text.txt","r")
 # print(file.readline())
 # print(file.readline())
 # print(file.readline())
-for i in file:
-    print(i)
+# for i in file:
+#     print(i)
+file1= open("text.txt","r")
+file2= open("abc.txt","w")
+for line in file1.readlines():
+    if not (line.startswith("coding")):
+        print(line)
+        file2.write(line)
