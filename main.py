db174b6c40d975a718ce9688016305f2b8436b95
Arshey123 <aseemsharma1980@gmail.com>
2024-10-09 20:03:07 +0530

    file handling part 1

---
+++ main.py
@@ -0,0 +1,12 @@
+# activity 1
+# file= open("text.txt","r")
+# print(file.read(30))
+# file= open("text.txt","a")
+# file.write("Beyblad x is coming")
+# activity 2
+file= open("text.txt","r")
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+for i in file:
+    print(i)
